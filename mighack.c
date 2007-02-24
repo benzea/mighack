ddba21b946ad01162d1f5b3a1584dfe53d6a4f80
Benjamin Berg <benjamin@sipsolutions.net>
2007-02-24 16:40:44 +0100

    Saner screen name sanitation that does not use any buffers.

--- mighack.c
+++ mighack.c
@@ -20,84 +20,90 @@ static guint my_signal = 0;
  * result must point to a buffer long enough to hold
  * the whole dname plus at least two characters more!
  */
-static void sanitize_display_name(const char *dname, char *result)
+static gchar *
+sanitize_display_name (const gchar *dname)
 {
-  const char *res = dname;
-  char *tmp;
+  const gchar *res = dname;
+  gchar *tmp;
 
   g_assert(dname);
 
-  /* split off leading "localhost" */
+  /* skip leading "localhost" */
   if (g_str_has_prefix (res, "localhost"))
     {
       res += 9;
     }
 
   tmp = g_strrstr (res, ":");
-  /* something's wrong with it, but hey... */
+  /* something's wrong with it, just return the original */
   if (!tmp)
     {
-      strcpy (result, res);
-      return;
+      return g_strdup (dname);
     }
 
   /* if it has a trailing .X then return */
   if (strstr (tmp, "."))
     {
-      strcpy (result, res);
-      return;
+      return g_strdup (res);
     }
 
-  strcpy (result, res);
-  /* add trailing .0 */
-  strcat(result, ".0");
+  return g_strconcat (res, ".0", NULL);
 }
 
 static void
 my_window_set_screen (GObject *object, const gchar *display_str)
 {
   gchar **cycle_displays;
-  const gchar *tmpstr;
   GSList *item, *list;
   GdkDisplay *display = NULL;
   GdkScreen *screen;
-  gchar cur_display[1024], buf[1024], switch_to[1024];
+  gchar *cur_display, *switch_to = NULL;
   int i;
 
   if (display_str == NULL)
     return;
 
-  tmpstr = gdk_display_get_name (gdk_screen_get_display (gtk_window_get_screen (GTK_WINDOW (object))));
-  sanitize_display_name (tmpstr, cur_display);
+  cur_display = (gchar*) gdk_display_get_name (gdk_screen_get_display (gtk_window_get_screen (GTK_WINDOW (object))));
+  cur_display = sanitize_display_name (cur_display);
 
   cycle_displays = g_strsplit_set (display_str, "#,;", 0);
 
   /* now try to find the display we are on */
   for (i = 0; cycle_displays[i]; i++)
     {
-      sanitize_display_name (cycle_displays[i], buf);
-      if (g_str_equal (buf, cur_display))
+      gchar *cycle_display;
+      cycle_display = sanitize_display_name (cycle_displays[i]);
+
+      if (g_str_equal (cycle_display, cur_display))
         {
+          gchar *next;
+
           /* if i+1 is the end then we need to wrap around */
-          tmpstr = cycle_displays[i+1];
-          if (!tmpstr)
-            tmpstr = cycle_displays[0];
-          sanitize_display_name(tmpstr, switch_to);
+          next = cycle_displays[i+1];
+          if (!next)
+            next = cycle_displays[0];
+          switch_to = sanitize_display_name (next);
         }
+
+      g_free (cycle_display);
     }
+  g_free (cur_display);
 
   list = gdk_display_manager_list_displays (gdk_display_manager_get ());
   for (item = list; item; item = g_slist_next (item))
     {
-      GdkDisplay *tmp = (GdkDisplay*) item->data;
+      gchar *display_name;
+      GdkDisplay *data = (GdkDisplay*) item->data;
 
-      sanitize_display_name (gdk_display_get_name (tmp), buf);
+      display_name = sanitize_display_name (gdk_display_get_name (data));
 
-      if (g_str_equal (buf, switch_to))
+      if (g_str_equal (display_name, switch_to))
         {
-          display = tmp;
+          display = data;
+          g_free (display_name);
           break;
         }
+      g_free (display_name);
     }
   g_slist_free (list);
 
@@ -117,6 +123,7 @@ my_window_set_screen (GObject *object, const gchar *display_str)
   gtk_window_set_screen (GTK_WINDOW (object), screen);
 
  out_free:
+  g_free (switch_to);
   g_strfreev (cycle_displays);
 }
 
